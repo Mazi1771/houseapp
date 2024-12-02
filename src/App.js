514fef48488f7c7a2a9eba7b56a0fbe80d2b0dc4
Mazi1771 <mazi1771@gmail.com>
2024-12-02 21:10:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -956,12 +956,12 @@ const handleRegister = (data) => {
 
       {/* Przycisk udostępniania */}
       <button
-        onClick={() => setShareModalOpen(true)}
-        className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
-      >
-        <Share className="h-4 w-4" />
-        Udostępnij Tablicę
-      </button>
+  onClick={() => setShareModalOpen(true)}  // Na to
+  className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+>
+  <Share className="h-4 w-4" />
+  Udostępnij Tablicę
+</button>
 
       {/* Przycisk przełączania widoku */}
       <button
